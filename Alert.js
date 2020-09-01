cd822593fd93c5b026e8e27276b0803e9e789a1b
SH985 <69173024+SH985@users.noreply.github.com>
2020-08-31 20:17:12 -0500

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,10 @@
+swal.fire({
+  title: "Estamos en remodelación",
+  text: "Te invitamos a revisar nuestra pagina de Facebook o nuestra cuenta de instagram. Podras acceder a ellos desde la pagina de inicio",
+  paddin: "1rem",
+  backdrop: "true",
+  toast:"true",
+  position:"top",
+  icon: "warning",
+  confirmButtonText: "Ok",
+});
